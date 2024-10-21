5d0f59f3d7edaa83fd1fbaacaa9d8fde023104cb
“vadvolo” <volovikvd@gmail.com>
2024-10-22 00:14:32 +0200

    update

--- lab/dynamips/install.sh
+++ lab/dynamips/install.sh
@@ -4,7 +4,6 @@ apt update
 apt install -y sudo ssh dynamips dynagen telnet telnetd net-tools bridge-utils iproute2 uml-utilities openvpn inetutils-ping wget
 
 mkdir /home/ubuntu/ios-7200
-mkdir /home/ubuntu/next-hop-lab
 wget -P /home/ubuntu/ios-7200 ftp://ftp.radio-portal.su/Cisco/\!cisco_ios/7200/c7200-jk9s-mz.124-13a.bin
 
 ip tuntap add tap0 mode tap
@@ -27,6 +26,7 @@ ifconfig br0 172.20.0.20/16
 
 cd /home/ubuntu/
 dynamips -H 7200 &
+sleep 3s
 dynagen lab.net
 
 tail -f /dev/null
