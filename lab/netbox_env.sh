00e10a85ccc477c817773e9f93ba635ebbcbb2d4
“vadvolo” <volovikvd@gmail.com>
2024-10-21 21:41:10 +0200

    update

--- lab/netbox_env.sh
+++ lab/netbox_env.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 PORT=":8080"
-IP=`echo $(docker exec -it netbox-docker-netbox-1 bash -c "ifconfig eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}'")`
+# IP=`echo $(docker exec -u root -it netbox-docker-netbox-1 bash -c "apt upgrade && apt install net-tools && echo "\n\n" && ifconfig eth0 | grep inet | grep -oP '(?<=inet\s)\d+(\.\d+){3}'")`
+IP=`echo $(docker inspect netbox-docker-netbox-1 | grep "IPAddress" | tail -n 1 | awk '{print $2}' | tr -d '",')`
 
 export NETBOX_IP=$IP
 export NETBOX_URL="http://"${IP:0:${#IP}-1}":8080"
@@ -11,4 +12,6 @@ echo $NETBOX_IP
 echo $NETBOX_URL
 echo $NETBOX_PORT
 
-
+cd ./netbox-docker
+docker restart netbox-docker-annet-1
+cd ..
