2d6bb834e6c4b79e67dc385814e5eceffbcbfde4
Grigorii Solovev <gslv@yandex-team.ru>
2024-11-01 16:51:33 +0300

    add lab-dc cisco drain

--- lab-dc/annet/lab_generators/rpl.py
+++ lab-dc/annet/lab_generators/rpl.py
@@ -16,12 +16,17 @@ class RoutePolicy(PartialGenerator):
     
     def run_cisco(self, device: Device):
         yield "ip bgp-community new-format"
+        yield "ip community-list standard TOR_NETS permit 65000:1"
+        yield "ip community-list standard GRACEFUL_SHUTDOWN permit 65535:0"
 
         if device.device_role.name == "ToR":
             yield """
-ip community-list standard TOR_NETS permit 65000:1
 route-map SPINE_IMPORT permit 10
+  match community TOR_NETS GRACEFUL_SHUTDOWN
+  set local-preference 0
+route-map SPINE_IMPORT permit 20
   match community TOR_NETS
+  set local-preference 100
 route-map SPINE_IMPORT deny 9999
 route-map SPINE_EXPORT permit 10
   match community TOR_NETS
@@ -33,11 +38,18 @@ route-map CONNECTED deny 9999
 """
         elif device.device_role.name == "Spine":
             yield """
-ip community-list standard TOR_NETS permit 65000:1
 route-map TOR_IMPORT permit 10
   match community TOR_NETS
 route-map TOR_IMPORT deny 9999
-route-map TOR_EXPORT permit 10
-  match community TOR_NETS
-route-map TOR_EXPORT deny 9999
 """
+            with self.block("route-map TOR_EXPORT permit 10"):
+                yield "match community TOR_NETS"
+                if is_drained_device(device):
+                    yield "set community 65535:0 additive"
+            yield "route-map TOR_EXPORT deny 9999"
+
+
+def is_drained_device(device: Device) -> bool:
+    if "maintenance" in [tag.name for tag in device.tags]:
+        return True
+    return False
