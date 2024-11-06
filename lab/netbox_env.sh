6b4574205a5de71d4bed19ab60578d86f07f8631
Grigorii Solovev <gslv@yandex-team.ru>
2024-11-06 11:20:33 +0300

    some stuff

--- lab/netbox_env.sh
+++ lab/netbox_env.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-PORT=":8080"
-# IP=`echo $(docker exec -u root -it netbox-docker-netbox-1 bash -c "apt upgrade && apt install net-tools && echo "\n\n" && ifconfig eth0 | grep inet | grep -oP '(?<=inet\s)\d+(\.\d+){3}'")`
-IP=`echo $(docker inspect netbox-docker-netbox-1 | grep "IPAddress" | tail -n 1 | awk '{print $2}' | tr -d '",')`
-
 export NETBOX_TOKEN="a630dcefcb191982869e7576190e79bfd569d33c"
-export NETBOX_IP=$IP
-export NETBOX_URL="http://"${IP}":8080"
+export NETBOX_URL="http://netbox-docker-netbox-1:8080"
 export NETBOX_PORT="8080"
--- lab/topologies/lab03_dc_cisco/README.md
+++ lab/topologies/lab03_dc_cisco/README.md
@@ -2,7 +2,7 @@
 
 ## Preparation
 
-Before you start the lab please place in the `src/ios-7200` folder IOL image `c7200-jk9s-mz.124-13a.bin`.
+Before you start please put into `../vm_images` Cisco IOS image `c7200-jk9s-mz.124-13a.bin`
 
 ## Description
 
