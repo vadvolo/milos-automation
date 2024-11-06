f0162c96adb03d0b0e4339caac9221e9a5b1772c
“vadvolo” <volovikvd@gmail.com>
2024-11-06 10:20:21 +0100

    fixes

--- lab/topologies/lab01_frr-only-test/src/my_generators/interfaces.py
+++ lab/topologies/lab01_frr-only-test/src/my_generators/interfaces.py
@@ -7,13 +7,14 @@ class IfaceDescriptions(PartialGenerator):
     
     TAGS = ["description"]
     
-    def acl_cisco(self, device):
+    def acl_mellanox(self, device):
         return """
         interface
             description
         """
     
-    def run_cisco(self, device):
+    def run_mellanox(self, device):
+        print(device)
         for interface in device.interfaces:
             neighbor = ""
             if interface.connected_endpoints:
@@ -29,13 +30,13 @@ class IfaceMtu(PartialGenerator):
     
     TAGS = ["description"]
     
-    def acl_cisco(self, device):
+    def acl_mellanox(self, device):
         return """
         interface
             mtu
         """
     
-    def run_cisco(self, device):
+    def run_mellanox(self, device):
         for interface in device.interfaces:
             if interface.mtu:
                 mtu = interface.mtu
@@ -43,47 +44,3 @@ class IfaceMtu(PartialGenerator):
                 mtu = MTU
             with self.block(f"interface {interface.name}"):
                 yield f"mtu {mtu}"
-
-class Ifaces(PartialGenerator):
-    TAGS = ["mgmt", "lldp"]
-
-    def acl_routeros(self, _):
-        return """
-        ip
-          neighbor              %cant_delete
-            discovery-settings  %cant_delete
-              set               %cant_delete
-
-        """
-
-    def run_routeros(self, _: Device):
-        with self.block("ip"):
-            with self.block("neighbor"):
-                with self.block("discovery-settings"):
-                    yield "set discover-interface-list=LAN lldp-med-net-policy-vlan=2 protocol=cdp"
-
-
-class Ntp(PartialGenerator):
-    TAGS = ["mgmt", "ntp"]
-
-    def acl_routeros(self, _):
-        return """
-        system
-          ntp         %cant_delete
-            client    %cant_delete
-              set     %cant_delete
-              server  %cant_delete
-                add   %cant_delete
-        """
-
-    def run_routeros(self, device: Device):
-        with self.block("system"):
-            with self.block("ntp"):
-                with self.block("client"):
-                    yield "set enabled=yes"
-
-        with self.block("system"):
-            with self.block("ntp"):
-                with self.block("client"):
-                    with self.block("server"):
-                        yield "add address=ntp0.ntp-servers.net"
